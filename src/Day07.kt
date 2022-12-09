91188bfd6ab8ec91cbd877abaff8476f02bcca82
Andrew Fulcher <andrew.fulcher@gmail.com>
2022-12-09 18:41:24 +0000

    Removed test input

--- src/Day07.kt
+++ src/Day07.kt
@@ -13,31 +13,6 @@ data class File(
     val name: String,
 ): Node()
 
-// Test output
-// $ cd /
-// $ ls
-// dir a
-// 14848514 b.txt
-// 8504156 c.dat
-// dir d
-// $ cd a
-// $ ls
-// dir e
-// 29116 f
-// 2557 g
-// 62596 h.lst
-// $ cd e
-// $ ls
-// 584 i
-// $ cd ..
-// $ cd ..
-// $ cd d
-// $ ls
-// 4060174 j
-// 8033020 d.log
-// 5626152 d.ext
-// 7214296 k
-
 fun main() {
     fun Directory.findDirectory(name: String): Directory {
         return nodes.first { it is Directory && it.name == name } as Directory
